192ed5a6fc79e3ff9aaa4673e9458ff10e33792f
amir abkhoshk <amirabkhoshk@gmail.com>
2026-07-10 16:13:39 +0200

    feat(core): Add telemetry subscriptions and main loop

--- gateway/src/main.cpp
+++ gateway/src/main.cpp
@@ -2,14 +2,18 @@
 // Created by amir abkhoshk on 09/07/2026.
 //
 
+#include <chrono>
 #include <cstdlib>
+#include <thread>
 
 #include <spdlog/spdlog.h>
 
+#include "telemetry.h"
 #include "vehicle.h"
 
 namespace {
 constexpr auto kConnectionUrl = "udp://:14540";
+constexpr float kPositionRateHz = 1.0f;
 }  // namespace
 
 int main() {
@@ -21,11 +25,17 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    spdlog::info(
-        "connected to {} (is_connected={})", vehicle.get_drone_url(), vehicle.is_connected());
+    spdlog::info("connected to {}", vehicle.get_drone_url());
 
-    vehicle.disconnect();
-    spdlog::info("is_connected after disconnect: {}", vehicle.is_connected());
+    TelemetryInfo telemetry(vehicle);
+    telemetry.set_telemetry_rate(kPositionRateHz);
+    telemetry.subscribe_drone_position();
+    telemetry.subscribe_drone_battery();
 
+    while (vehicle.is_connected()) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    spdlog::warn("link lost, exiting");
     return EXIT_SUCCESS;
 }
